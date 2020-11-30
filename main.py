98ecfc0a9fd79dd63b292bc133d09f9714ec3246
Anthony Stephens <antwon97@gmail.com>
2020-11-30 16:39:15 -0600

    Remove extraneous code and comments

--- main.py
+++ main.py
@@ -3,63 +3,6 @@ app = Flask('app', root_path='./')
 
 placeholderList = {"userOld": "", "userError": "", "pwdError": "", "emailOld": "", "emailError": ""}
 
-userForm = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <style>
-            form {{
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 60%;
-                font: 16px sans-serif;
-                border-radius: 10px;
-                text-align: center;
-            }}
-            label {{
-                display:inline-block;
-                float: left;
-                padding-top: 5px;
-                text-align: right;
-                width: 35%;
-            }}
-            input {{
-                margin-top: 5px;
-                display:inline-block;
-                width: 45%
-            }}      
-        </style>
-    </head>
-    <body>
-      <h1 style="text-align: center"> User Signup </h1>
-      <form method="post">
-        <label> Username*: </label>
-        <input type="text" name="username" minlength="3" maxlength="20" required value="{userOld}"/>
-        </br>
-        <i>{userError}</i>
-
-        <label> Password*:</label>
-        <input type="password" name="pworda" minlength="8" required/>
-        </br>
-        <i>{pwdError}</i>
-
-        <label> Retype Password*:</label>
-        <input type="password" name="pwordb" minlength="8" required/>
-        </br>
-
-        <label> Email Address: </label>
-        <input type="text" name="email" value="{emailOld}"/>
-        </br>
-        <i>{emailError}</i>
-        </br>
-
-        <input type="submit" value="Signup"/>
-      </form>
-    </body>
-</html>
-"""
-
 
 @app.route("/", methods=['POST'])
 def checkInput():
@@ -76,11 +19,12 @@ def checkInput():
     pwdMsg = ""
     emailMsg = ""
 
+    # Flask seems to do validation for input length, and displays related errors
+
     if (inUsername.find(' ') != -1):
       userMsg = "Usernames cannot contain spaces"
 
     if (inPwdA != inPwdB):
-      print("Mismatched Passwords Error")
       pwdMsg += "Passwords must match"
     elif (inEmail.find(' ') != -1):
       if (pwdMsg != ""):
@@ -120,17 +64,13 @@ def checkInput():
 @app.route('/welcome/<username>')
 def welcome(username):
 
-    print(username)
-
     return render_template('welcome.html', userNm=username)
-    #return welcomeForm.format(userNm=username)
 
 
 @app.route("/")
 def index():
 
     return render_template('index.html', plcList=placeholderList)
-    #return userForm.format(userOld="", userError="", pwdError="", emailOld="", emailError="")
 
 
 app.run()
